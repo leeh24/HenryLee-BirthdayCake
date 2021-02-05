a55243042c764dc2e88e6245825e3588f2e57523
leeh24 <leeh24@up.edu>
2021-02-04 19:50:43 -0800

    Draw two candles equidistant from each other

--- BirthdayCake-master/app/src/main/java/cs301/birthdaycake/CakeView.java
+++ BirthdayCake-master/app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -119,9 +119,9 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
-
+        //Draw 2 candles equidistant from each other
+        drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/3, cakeTop);
+        drawCandle(canvas, cakeLeft + 2*cakeWidth/3 - 2*candleWidth/3, cakeTop);
     }//onDraw
 
 }//class CakeView
