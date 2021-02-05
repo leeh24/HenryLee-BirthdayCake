386534c1416b8773858283830a64a076e8e3b8d9
leeh24 <leeh24@up.edu>
2021-02-04 19:39:48 -0800

    Check in Birthday Cake

--- BirthdayCake-master/app/src/main/java/cs301/birthdaycake/CakeView.java
+++ BirthdayCake-master/app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -119,9 +119,8 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the left and right
-        drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/3, cakeTop);
-        drawCandle(canvas, cakeLeft + 2*cakeWidth/3 - 2*candleWidth/3, cakeTop);
+        //Now a candle in the center
+        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
 
     }//onDraw
 
